a129a3fc40f34932370de13d20fa7cb60a2a291c
Blake Ramsdell <blaker@gmail.com>
2022-10-11 14:26:41 -0700

    Yank access specifiers

--- Bcr.BluesWireless.Notecard.Core/ICommunicationChannel.cs
+++ Bcr.BluesWireless.Notecard.Core/ICommunicationChannel.cs
@@ -5,10 +5,10 @@ interface ICommunicationChannel
     /// <summary>
     /// Send a line. Do NOT include the trailing LF in your data.
     /// </summary>
-    public void SendLine(string data);
+    void SendLine(string data);
 
     /// <summary>
     /// Receive a line. The trailing LF is NOT included.
     /// </summary>
-    public string ReceiveLine();
+    string ReceiveLine();
 }

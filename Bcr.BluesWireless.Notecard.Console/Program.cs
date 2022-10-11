bf2d41f0808e0b2d7ab5d3148d2492607e2e4a7f
Blake Ramsdell <blaker@gmail.com>
2022-10-11 12:11:49 -0700

    Add desktop serial port communications

--- Bcr.BluesWireless.Notecard.Console/Program.cs
+++ Bcr.BluesWireless.Notecard.Console/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO.Ports;
+using System.Text.RegularExpressions;
+
+IEnumerable<string> GetPotentialSerialPortNames()
+{
+    return SerialPort.GetPortNames().Where((name) => Regex.IsMatch(name, "cu.*NOTE.*"));
+}
+
+using (var serialPort = new SerialPort(GetPotentialSerialPortNames().First()))
+{
+    serialPort.Open();
+    serialPort.WriteLine("{\"req\":\"card.time\"}");
+    Console.WriteLine(serialPort.ReadLine());
+}

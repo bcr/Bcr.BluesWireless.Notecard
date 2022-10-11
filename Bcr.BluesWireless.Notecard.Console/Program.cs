1ef18ed721a7e4a4476213d8b8d7420e11c9eac5
Blake Ramsdell <blaker@gmail.com>
2022-10-11 16:35:37 -0700

    Make console app more REPLish

--- Bcr.BluesWireless.Notecard.Console/Program.cs
+++ Bcr.BluesWireless.Notecard.Console/Program.cs
@@ -13,6 +13,15 @@ using (var serialPort = new SerialPort(GetPotentialSerialPortNames().First()))
     serialPort.Open();
     var communicationChannel = new SerialPortCommunicationChannel(serialPort);
 
-    communicationChannel.SendLine("{\"req\":\"card.time\"}");
-    Console.WriteLine(communicationChannel.ReceiveLine());
+    while (true)
+    {
+        Console.Write("> ");
+        var command = Console.ReadLine();
+        if (command == null)
+        {
+            break;
+        }
+        communicationChannel.SendLine(command);
+        Console.WriteLine(communicationChannel.ReceiveLine());
+    }
 }

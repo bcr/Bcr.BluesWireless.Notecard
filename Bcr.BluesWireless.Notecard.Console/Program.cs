a7f438547c6bfa17225cec1b4e0b5c53fc6c76a4
Blake Ramsdell <blaker@gmail.com>
2022-10-12 23:13:16 -0700

    Convert to traditional static Main

--- Bcr.BluesWireless.Notecard.Console/Program.cs
+++ Bcr.BluesWireless.Notecard.Console/Program.cs
@@ -8,88 +8,94 @@ using System.Text;
 using System.Text.Json;
 using System.Text.RegularExpressions;
 
-IEnumerable<string> GetPotentialSerialPortNames()
+internal class Program
 {
-    return SerialPort.GetPortNames().Where((name) => Regex.IsMatch(name, "cu.*NOTE.*"));
-}
-
-DateTimeOffset MakeDateTimeOffset(long unixTime, int offsetMinutes)
-{
-    var offset = DateTimeOffset.FromUnixTimeSeconds(unixTime);
-    offset = offset.AddMinutes(offsetMinutes);
-    offset = new DateTimeOffset(offset.DateTime, TimeSpan.FromMinutes(offsetMinutes));
-
-    return offset;
-}
-
-string GetHistoryPath()
-{
-    return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), ".bluehistory");
-}
-
-void OutputFormattedJson(IConsole console, string json)
-{
-    var reader = new Utf8JsonReader(Encoding.UTF8.GetBytes(json));
-    var styleDictionary = new Dictionary<JsonTokenType, AnsiColor> {
-        { JsonTokenType.String, AnsiColor.BrightGreen },
-        { JsonTokenType.Number, AnsiColor.Yellow },
-    };
-    var formatting = new List<FormatSpan>();
-
-    while (reader.Read())
+    static IEnumerable<string> GetPotentialSerialPortNames()
     {
-        int start = (int) reader.TokenStartIndex;
-        int length = reader.HasValueSequence ? (int) reader.ValueSequence.Length : reader.ValueSpan.Length;
-
-        switch (reader.TokenType)
-        {
-            case JsonTokenType.PropertyName:
-            case JsonTokenType.String:
-                length += 2;
-                break;
-        }
-
-        if (styleDictionary.ContainsKey(reader.TokenType))
-        {
-            formatting.Add(new FormatSpan(start, length, styleDictionary[reader.TokenType]));
-        }
+        return SerialPort.GetPortNames().Where((name) => Regex.IsMatch(name, "cu.*NOTE.*"));
     }
 
-    console.WriteLine(new FormattedString(json, formatting));
-}
-
-using (var serialPort = new SerialPort(GetPotentialSerialPortNames().First()))
-{
-    serialPort.Open();
-    Console.WriteLine($"Connected to {serialPort.PortName}");
-    var communicationChannel = new SerialPortCommunicationChannel(serialPort);
-    var historyPath = GetHistoryPath();
-    var console = new SystemConsole();
-    var prompt = new Prompt(persistentHistoryFilepath: historyPath, configuration: new PromptConfiguration(prompt: "> "), console: console);
-    Console.WriteLine($"History saved at {historyPath}");
-
-    while (true)
+    static DateTimeOffset MakeDateTimeOffset(long unixTime, int offsetMinutes)
     {
-        var response = await prompt.ReadLineAsync();
-        if ((!response.IsSuccess) || (response.Text == "exit"))
+        var offset = DateTimeOffset.FromUnixTimeSeconds(unixTime);
+        offset = offset.AddMinutes(offsetMinutes);
+        offset = new DateTimeOffset(offset.DateTime, TimeSpan.FromMinutes(offsetMinutes));
+
+        return offset;
+    }
+
+    static string GetHistoryPath()
+    {
+        return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), ".bluehistory");
+    }
+
+    static void OutputFormattedJson(IConsole console, string json)
+    {
+        var reader = new Utf8JsonReader(Encoding.UTF8.GetBytes(json));
+        var styleDictionary = new Dictionary<JsonTokenType, AnsiColor> {
+            { JsonTokenType.String, AnsiColor.BrightGreen },
+            { JsonTokenType.Number, AnsiColor.Yellow },
+        };
+        var formatting = new List<FormatSpan>();
+
+        while (reader.Read())
         {
-            break;
+            int start = (int) reader.TokenStartIndex;
+            int length = reader.HasValueSequence ? (int) reader.ValueSequence.Length : reader.ValueSpan.Length;
+
+            switch (reader.TokenType)
+            {
+                case JsonTokenType.PropertyName:
+                case JsonTokenType.String:
+                    length += 2;
+                    break;
+            }
+
+            if (styleDictionary.ContainsKey(reader.TokenType))
+            {
+                formatting.Add(new FormatSpan(start, length, styleDictionary[reader.TokenType]));
+            }
         }
 
-        communicationChannel.SendLine(response.Text);
-        var receivedLine = communicationChannel.ReceiveLine();
-        OutputFormattedJson(console, receivedLine);
-        if (response.Text.Contains("card.time"))
+        console.WriteLine(new FormattedString(json, formatting));
+    }
+
+    private static async Task Main(string[] args)
+    {
+        using (var serialPort = new SerialPort(GetPotentialSerialPortNames().First()))
         {
-            var json = JsonSerializer.Deserialize<CardTimeResponse>(
-                receivedLine,
-                new JsonSerializerOptions {
-                    PropertyNamingPolicy = new InitialCapNamingPolicy()
-                    }
-                );
-            Console.WriteLine(json);
-            var offset = MakeDateTimeOffset(json!.Time, json.Minutes);
-            Console.WriteLine(offset);
+            serialPort.Open();
+            Console.WriteLine($"Connected to {serialPort.PortName}");
+            var communicationChannel = new SerialPortCommunicationChannel(serialPort);
+            var historyPath = GetHistoryPath();
+            var console = new SystemConsole();
+            var prompt = new Prompt(persistentHistoryFilepath: historyPath, configuration: new PromptConfiguration(prompt: "> "), console: console);
+            Console.WriteLine($"History saved at {historyPath}");
+
+            while (true)
+            {
+                var response = await prompt.ReadLineAsync();
+                if ((!response.IsSuccess) || (response.Text == "exit"))
+                {
+                    break;
+                }
+
+                communicationChannel.SendLine(response.Text);
+                var receivedLine = communicationChannel.ReceiveLine();
+                OutputFormattedJson(console, receivedLine);
+                if (response.Text.Contains("card.time"))
+                {
+                    var json = JsonSerializer.Deserialize<CardTimeResponse>(
+                        receivedLine,
+                        new JsonSerializerOptions {
+                            PropertyNamingPolicy = new InitialCapNamingPolicy()
+                        }
+                        );
+                    Console.WriteLine(json);
+                    var offset = MakeDateTimeOffset(json!.Time, json.Minutes);
+                    Console.WriteLine(offset);
+                }
+            }
         }
     }
 }

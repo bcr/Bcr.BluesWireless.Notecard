13f5f97c657253904a7408d854ba3646887e7d5a
Blake Ramsdell <blaker@gmail.com>
2022-10-13 01:18:50 -0700

    More completion work

--- Bcr.BluesWireless.Notecard.Console/Program.cs
+++ Bcr.BluesWireless.Notecard.Console/Program.cs
@@ -17,12 +17,21 @@ internal class Program
         { JsonTokenType.Number, AnsiColor.Yellow },
     };
 
-    static List<CompletionItem> _requestCompletionItems = new () {
-        new CompletionItem("{\"req\":\"card.status\"}", "card.status", getExtendedDescription: (_) => Task.FromResult<FormattedString>(new FormattedString("Returns general information about the Notecard's operating status."))),
-        new CompletionItem("{\"req\":\"card.temp\"}", "card.temp", getExtendedDescription: (_) => Task.FromResult<FormattedString>(new FormattedString("Get the current temperature from the Notecard's onboard calibrated temperature sensor."))),
-        new CompletionItem("note.add"),
-        new CompletionItem("note.changes"),
+    class NotecardCompletionItem
+    {
+        public string? Request { get; init; }
+        public string? Json { get; init; }
+        public string? Description { get; init; }
+    }
+
+    static List<NotecardCompletionItem> _notecardCompletionItems = new () {
+        new NotecardCompletionItem { Request = "card.status", Json = "{\"req\":\"card.status\"}", Description = "Returns general information about the Notecard's operating status." },
+        new NotecardCompletionItem { Request = "card.temp", Json = "{\"req\":\"card.temp\"}", Description = "Get the current temperature from the Notecard's onboard calibrated temperature sensor." },
+        new NotecardCompletionItem { Request = "note.add" },
+        new NotecardCompletionItem { Request = "note.changes" },
     };
+
+    static List<CompletionItem> _requestCompletionItems = new ();
 
     static IEnumerable<string> GetPotentialSerialPortNames()
     {
@@ -70,6 +79,16 @@ internal class Program
         console.WriteLine(new FormattedString(json, formatting));
     }
 
+    static void BuildRequestCompletionItems()
+    {
+        foreach (var item in _notecardCompletionItems)
+        {
+            _requestCompletionItems.Add(new CompletionItem(
+                item.Json ?? item.Request ?? "", item.Request, getExtendedDescription: ((item.Description != null) ? ((_) => Task.FromResult<FormattedString>(item.Description)) : null)
+            ));
+        }
+    }
+
     private class NotecardPromptCallbacks : PromptCallbacks
     {
         protected override Task<IReadOnlyList<CompletionItem>> GetCompletionItemsAsync(string text, int caret, TextSpan spanToBeReplaced, CancellationToken cancellationToken)
@@ -80,6 +99,7 @@ internal class Program
 
     private static async Task Main(string[] args)
     {
+        BuildRequestCompletionItems();
         using (var serialPort = new SerialPort(GetPotentialSerialPortNames().First()))
         {
             serialPort.Open();

4ed3bcd13fc1d5af810e77ab0f32f57db8b474d7
Blake Ramsdell <blaker@gmail.com>
2022-10-12 23:01:49 -0700

    Add JSON output formatting

--- Bcr.BluesWireless.Notecard.Console/Program.cs
+++ Bcr.BluesWireless.Notecard.Console/Program.cs
@@ -1,7 +1,10 @@
 ﻿using Bcr.BluesWireless.Notecard.Console;
 using Bcr.BluesWireless.Notecard.Core;
 using PrettyPrompt;
+using PrettyPrompt.Consoles;
+using PrettyPrompt.Highlighting;
 using System.IO.Ports;
+using System.Text;
 using System.Text.Json;
 using System.Text.RegularExpressions;
 
@@ -24,13 +27,45 @@ string GetHistoryPath()
     return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), ".bluehistory");
 }
 
+void OutputFormattedJson(IConsole console, string json)
+{
+    var reader = new Utf8JsonReader(Encoding.UTF8.GetBytes(json));
+    var styleDictionary = new Dictionary<JsonTokenType, AnsiColor> {
+        { JsonTokenType.String, AnsiColor.BrightGreen },
+        { JsonTokenType.Number, AnsiColor.Yellow },
+    };
+    var formatting = new List<FormatSpan>();
+
+    while (reader.Read())
+    {
+        int start = (int) reader.TokenStartIndex;
+        int length = reader.HasValueSequence ? (int) reader.ValueSequence.Length : reader.ValueSpan.Length;
+
+        switch (reader.TokenType)
+        {
+            case JsonTokenType.PropertyName:
+            case JsonTokenType.String:
+                length += 2;
+                break;
+        }
+
+        if (styleDictionary.ContainsKey(reader.TokenType))
+        {
+            formatting.Add(new FormatSpan(start, length, styleDictionary[reader.TokenType]));
+        }
+    }
+
+    console.WriteLine(new FormattedString(json, formatting));
+}
+
 using (var serialPort = new SerialPort(GetPotentialSerialPortNames().First()))
 {
     serialPort.Open();
     Console.WriteLine($"Connected to {serialPort.PortName}");
     var communicationChannel = new SerialPortCommunicationChannel(serialPort);
     var historyPath = GetHistoryPath();
-    var prompt = new Prompt(persistentHistoryFilepath: historyPath, configuration: new PromptConfiguration(prompt: "> "));
+    var console = new SystemConsole();
+    var prompt = new Prompt(persistentHistoryFilepath: historyPath, configuration: new PromptConfiguration(prompt: "> "), console: console);
     Console.WriteLine($"History saved at {historyPath}");
 
     while (true)
@@ -43,7 +78,7 @@ using (var serialPort = new SerialPort(GetPotentialSerialPortNames().First()))
 
         communicationChannel.SendLine(response.Text);
         var receivedLine = communicationChannel.ReceiveLine();
-        Console.WriteLine(receivedLine);
+        OutputFormattedJson(console, receivedLine);
         if (response.Text.Contains("card.time"))
         {
             var json = JsonSerializer.Deserialize<CardTimeResponse>(
